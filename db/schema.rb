3138d59f30a67a3666476d80c3219109342a089c
SPrio <mahantasupriyo@gmail.com>
2021-05-09 13:38:09 +0530

    updated Schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,10 +15,10 @@ ActiveRecord::Schema.define(version: 2021_05_06_105356) do
   create_table "messages", force: :cascade do |t|
     t.text "content"
     t.integer "user_id", null: false
-    t.integer "roon_id", null: false
+    t.integer "room_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["roon_id"], name: "index_messages_on_roon_id"
+    t.index ["room_id"], name: "index_messages_on_room_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
@@ -34,6 +34,6 @@ ActiveRecord::Schema.define(version: 2021_05_06_105356) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "messages", "roons"
+  add_foreign_key "messages", "rooms"
   add_foreign_key "messages", "users"
 end
